73c74ecf4fc94435cdda09f13aabb9c2d706b595
GreyXW <gration@advsysunlimited.com>
2020-02-07 10:53:56 -0600

    ASU google places change

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\src\fluttersdk\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\greyx\source\asu_flutter\flutter_google_places\example"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\src\fluttersdk\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
